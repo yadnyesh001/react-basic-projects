ddb41f77a5a63af263a18110aa2d0b42c0d24893
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-01-01 11:23:49 +0530

    tree-view

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ImageSlider from './components/image-slider';
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 import LoadMoreData from "./components/load-more-data";
+import TreeView from "./components/tree-view";
+import menus from "./components/tree-view/data";
 
 function App() {
   return (
@@ -16,7 +18,8 @@ function App() {
         page={"1"}
         limit={"10"}
       /> */}
-      <LoadMoreData/>
+      {/* <LoadMoreData/> */}
+      <TreeView menus={menus} />
     </div>
   );
 }
